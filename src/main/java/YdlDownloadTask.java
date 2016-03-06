62153df77cde57288094beb61ec47501ff7b9bb0
lm <mueller.lutz@gmail.com>
2016-03-06 14:10:27 +0100

    Initial commit

---
+++ src/main/java/YdlDownloadTask.java
@@ -0,0 +1,13 @@
+/**
+ * Execute youtube-dl download
+ *
+ * Created by lm on 06.03.16.
+ */
+public class YdlDownloadTask {
+
+	public final String url;
+
+	public YdlDownloadTask(String url) {
+		this.url = url;
+	}
+}
