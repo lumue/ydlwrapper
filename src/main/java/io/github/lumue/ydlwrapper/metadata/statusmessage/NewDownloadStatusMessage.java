5304fa30dc5770fc0f9c82d794d5a60266284ab9
lumue <ddlom85s>
2016-03-16 18:02:41 +0100

    fix parse error

--- src/main/java/io/github/lumue/ydlwrapper/metadata/statusmessage/NewDownloadStatusMessage.java
+++ src/main/java/io/github/lumue/ydlwrapper/metadata/statusmessage/NewDownloadStatusMessage.java
@@ -15,8 +15,8 @@ public class NewDownloadStatusMessage extends YdlStatusMessage{
 	private static final int NEW_FILE_STATUS_PREFIX_LENGTH = NEW_FILE_STATUS_PREFIX.length();
 
 	private final static Function<String,String> FILENAME_PARSER= line -> line.substring(NEW_FILE_STATUS_PREFIX_LENGTH);
-	private final static Function<String,String> EXTENSION_PARSER= line -> line.substring(line.lastIndexOf('.') +1);
-	private static final Function<String,String> FORMAT_ID_PARSER = line -> line.substring(line.lastIndexOf(".f") +1,line.lastIndexOf('.'));;
+	private final static Function<String,String> EXTENSION_PARSER= line -> line.substring(line.lastIndexOf('.') +1).replaceAll("\"","");
+	private static final Function<String,String> FORMAT_ID_PARSER = line -> line.substring(line.lastIndexOf(".f") +2,line.lastIndexOf('.'));;
 
 	private Supplier<String> filenameSupplier= lazily(() -> filenameSupplier=value(FILENAME_PARSER.apply(getLine())));
 	private Supplier<String> extensionSupplier= lazily(() -> filenameSupplier=value(EXTENSION_PARSER.apply(getLine())));
