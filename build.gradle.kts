3c1d4846dc82e5a13ac05bfbfdbcc41a5922afaa
lm <lm@lumue.net>
2024-01-06 18:47:27 +0100

    Update dependencies and add integration test resources in build.gradle.kts
    This commit updates the slf4j version used in build.gradle.kts and also adds srcDirs for integration-test resources in the sourceSets. Additionally, we've included the creation of a new log4j properties file, which will be used to manage logging during integration testing. These changes help to streamline the integration testing set up and improve the accuracy of logging.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 sourceSets {
     create("intTest") {
         java.srcDirs("src/integration-test/java")
+        resources.srcDirs("src/integration-test/resources")
         compileClasspath += sourceSets.main.get().output
         runtimeClasspath += sourceSets.main.get().output
     }
@@ -51,7 +52,7 @@ tasks.check { dependsOn(integrationTest) }
 dependencies {
 
     val validationApiVersion = "1.1.0.Final"
-    val slf4jApiVersion = "1.7.18"
+    val slf4jVersion = "2.0.10"
     val hamcrestAllVersion = "1.3"
     val commonsLang3Version = "3.4"
     val commonsIoVersion = "2.15.1"
@@ -59,13 +60,18 @@ dependencies {
     val junitVersion = "4.13.1"
 
     implementation("javax.validation:validation-api:${validationApiVersion}")
-    implementation("org.slf4j:slf4j-api:${slf4jApiVersion}")
+    implementation("org.slf4j:slf4j-api:${slf4jVersion}")
     implementation("org.hamcrest:hamcrest-all:${hamcrestAllVersion}")
     implementation("org.apache.commons:commons-lang3:${commonsLang3Version}")
     implementation("commons-io:commons-io:${commonsIoVersion}")
     implementation("com.fasterxml.jackson.core:jackson-core:${jacksonVersion}")
     implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
     implementation("com.fasterxml.jackson.core:jackson-annotations:${jacksonVersion}")
+
     testImplementation("junit:junit:${junitVersion}")
+
     intTestImplementation("junit:junit:${junitVersion}")
+
+    intTestRuntimeOnly("org.slf4j:slf4j-reload4j:${slf4jVersion}")
+
 }
